13cc559d2130d28d7ac99630abf2f1dd339c4743
Simon Baptiste <Baptiste.Simon@swisscom.com>
2019-07-16 09:13:37 +0200

    Post soutenance - docstrings / gitignore

--- foodbapp/myfoodapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Question, Food, Categories
-
-# Register your models here.
-
-admin.site.register(Question)
-admin.site.register(Food)
-admin.site.register(Categories)
